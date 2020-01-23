7c1239f4e4e834b17b710fad8bd4a9ec37953e47
Billy <billymcfly@gmail.com>
2020-01-23 15:09:56 -0700

    Add vue-cookie

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+var vue = require('vue');
+var vueCookie = require('vue-cookie');
+Vue.use(vueCookie);
+
 new Vue({
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
